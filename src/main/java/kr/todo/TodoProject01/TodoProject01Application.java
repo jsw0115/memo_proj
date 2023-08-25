67e8c47ee92158d3778ee0587f067691918fa1c9
jsw0115 <87688825+jsw0115@users.noreply.github.com>
2023-08-25 17:04:21 +0900

    Add files via upload
    프로젝트 시작

---
+++ src/main/java/kr/todo/TodoProject01/TodoProject01Application.java
@@ -0,0 +1,13 @@
+package kr.todo.TodoProject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoProject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/todo/TodoProject01/TodoProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.todo.TodoProject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
